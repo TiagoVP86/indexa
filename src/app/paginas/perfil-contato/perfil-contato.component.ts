61c2570e69e2a32751c3a61d2b2b89653a767ecd
Tiago Vieira <t.vp@hotmail.com>
2024-05-13 15:33:25 -0300

    Obtendo parâmetros das rotas

--- src/app/paginas/perfil-contato/perfil-contato.component.ts
+++ src/app/paginas/perfil-contato/perfil-contato.component.ts
@@ -1,22 +1,47 @@
-import { Component } from '@angular/core';
+import { ContatoService } from './../../services/contato.service';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContainerComponent } from '../../componentes/container/container.component';
 import { Contato } from '../../componentes/contato/contato';
+import { ActivatedRoute, RouterLink, Router } from '@angular/router';
 
 @Component({
   selector: 'app-perfil-contato',
   standalone: true,
   templateUrl: './perfil-contato.component.html',
   styleUrl: './perfil-contato.component.css',
-  imports: [CommonModule, ContainerComponent],
+  imports: [CommonModule, ContainerComponent, RouterLink],
 })
-export class PerfilContatoComponent {
+export class PerfilContatoComponent implements OnInit {
   contato: Contato = {
     id: 0,
-    nome: 'dev',
-    telefone: '213432432',
-    email: 'dev@email.com',
-    aniversario: '12/10/1990',
-    redes: 'facebook.com',
+    nome: '',
+    telefone: '',
+    email: '',
+    aniversario: '',
+    redes: '',
   };
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private contatoService: ContatoService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    if (id) {
+      this.contatoService.buscarPorId(parseInt(id)).subscribe((contato) => {
+        this.contato = contato;
+      });
+    }
+  }
+
+  excluir() {
+    if (this.contato.id) {
+      this.contatoService.excluirContato(this.contato.id).subscribe(() => {
+        this.router.navigateByUrl('/lista-contatos');
+      });
+    }
+  }
 }
